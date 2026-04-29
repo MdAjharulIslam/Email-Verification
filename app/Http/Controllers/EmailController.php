b8a1291ddb92982a9a14ff388de12f788b2d56d1
ajharul <mdajharulislam327@gmail.com>
2026-04-30 00:20:11 +0600

    about email

---
+++ app/Http/Controllers/EmailController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Mail\welcomeemail;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Mail;
+
+class EmailController extends Controller
+{
+    public function sendEmail(){
+        $toEmail = "ajharuli440@gmail.com";
+        $message = "Hello , welcome to our website";
+        $subject = "welcome";
+
+
+      $request =  Mail::to($toEmail)
+        ->send(new welcomeemail($message, $subject));
+    
+    dd($request);
+        }
+}
